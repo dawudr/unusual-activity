d09c567eea2659cecffcfa9668dd0b1a8a768947
Dawud Rahman <dawudr@gmail.com>
2017-12-07 21:09:59 +0000

    Revamp with Angular4_CLI_Starter template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,8 @@
-import {Component} from '@angular/core';
-
-import { AngularFireDatabase } from 'angularfire2/database';
-import { Observable } from 'rxjs/Observable';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  // tslint:disable-next-line
+  selector: 'body',
+  template: '<router-outlet></router-outlet>'
 })
-
-export class AppComponent {
-  title = 'Unusual Activity';
-  description = 'Unusual Activity';
-
-//  constructor() {}
-
-//  items: Observable<any[]>;
-//  constructor(db: AngularFireDatabase) {
-//    this.items = db.list('/stock').valueChanges();
-//  }
-
-}
+export class AppComponent { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,3 @@
 export const environment = {
-  production: true,
-  purchasedDB: 'https://stocks-dd1e5.firebaseio.com/purchase.json',  // restful database to save purchased list from google firebase
-  favoriteDB: 'https://stocks-dd1e5.firebaseio.com/favorite.json', // restful database to save favorite list from google firebase
-  configDB: 'https://stocks-dd1e5.firebaseio.com/config.json',// restful database to save config  from google firebase
-  apiKey: 1537,   // api key from  https://www.alphavantage.co
-  firebase: {
-    apiKey: "AIzaSyCLNfUWpTAPVky_NZVODh6oUPyEz3Jdcpg",
-    authDomain: "unusual-activity.firebaseapp.com",
-    databaseURL: "https://unusual-activity.firebaseio.com",
-    projectId: "unusual-activity",
-    storageBucket: "unusual-activity.appspot.com",
-    messagingSenderId: "742942868879"
-  }
+  production: true
 };
